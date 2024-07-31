28326d3ec282b59e7f7c726af4eb57f59ef17ca3
Magnus Larsen <magnushlarsen01@protonmail.com>
2024-07-31 11:39:35 +0200

    move columns

--- model/url.go
+++ model/url.go
@@ -1,17 +1,18 @@
 package model
 
 import (
+	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type Url struct {
+	Hash         string `gorm:"primaryKey"`
+	OriginalUrl  string
+	ShortenedUrl string
+	RunsOutAt    sql.NullTime
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
-	Hash         string         `gorm:"primaryKey"`
-	UrlToShorten string
-	ShortenedUrl string
-	RunsOutAt    time.Time
 }
